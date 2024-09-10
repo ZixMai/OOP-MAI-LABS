2ed78c0b31b2dd85091eb4c339cf979fb48e08e8
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-09-10 21:23:23 +0300

    remove

--- cmake-build-debug/lab_1/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/zix/WinBuntu/JetBrains_IDEs/clion_projects/OOP/lab_1
-# Build directory: /media/zix/WinBuntu/JetBrains_IDEs/clion_projects/OOP/cmake-build-debug/lab_1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Tests "/media/zix/WinBuntu/JetBrains_IDEs/clion_projects/OOP/cmake-build-debug/lab_1/tests")
-set_tests_properties(Tests PROPERTIES  _BACKTRACE_TRIPLES "/media/zix/WinBuntu/JetBrains_IDEs/clion_projects/OOP/lab_1/CMakeLists.txt;17;add_test;/media/zix/WinBuntu/JetBrains_IDEs/clion_projects/OOP/lab_1/CMakeLists.txt;0;")

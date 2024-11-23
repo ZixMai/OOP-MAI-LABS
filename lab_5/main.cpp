6225b806f233b9e63d2bcb1e07c7f286ab6c5863
Александр Жуховицкий <adzhukhovitskij@mai.education>
2024-11-23 23:05:28 +0300

    fix: remove unused

--- lab_5/main.cpp
+++ lab_5/main.cpp
@@ -5,8 +5,4 @@
 int main() {
   CustomMemoryResource customResource;
   std::pmr::polymorphic_allocator<int> allocator(&customResource);
-  LinkedList<int, std::pmr::polymorphic_allocator<int>> list(allocator);
-  list.push_front(1);
-  list.pop_front();
-  std::cout << list.front() << std::endl;
 }

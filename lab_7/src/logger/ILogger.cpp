18230febd4c1feb433e11c5d74ec02b704330ac2
Ivan Berezutskiy <santiago_bravo@mail.ru>
2025-10-05 15:54:36 +0300

    удалил ненужный код

--- lab_7/src/logger/ILogger.cpp
+++
@@ -1,4 +0,0 @@
-#include "ILogger.h"
-
-namespace Logger {
-}
--- lab_7/src/logger/console_logger.cpp
+++
@@ -1,8 +0,0 @@
-#include "console_logger.h"
-
-namespace Logger {
-    void ConsoleLogger::log(const std::string &str) const {
-        std::shared_lock lock(loggerMutex);
-        os << str << std::endl;
-    }
-}
